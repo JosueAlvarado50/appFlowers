ab532d462202c764cc0cbdc03d3aceabd2284114
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2022-11-04 11:29:07 -0600

    cambios

--- frontflowers/App.js
+++ frontflowers/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eaeaea",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 14,
+  },
+
+  title: {
+    marginTop: "-190%",
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    width: "100%",
   },
 });
